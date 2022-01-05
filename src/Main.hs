be88ae3d7b4f6e5eba3222f5c5efffb5dea934d8
Viktor Kleen <viktor@kleen.org>
2022-01-05 20:43:33 +0000

    Add namespacing to the deterministic search

--- src/Main.hs
+++ src/Main.hs
@@ -45,38 +45,38 @@ output n bs t1 t2 t3 = do
     t1 t2 t3
   hFlush stdout
 
-tryOnce :: Word64 -> (BS.ByteString, C.Element, C.Element, C.Element)
-tryOnce n =
+tryOnce :: BS.ByteString -> Word64 -> (BS.ByteString, C.Element, C.Element, C.Element)
+tryOnce ns n =
   let bs = BS.take 16 . convert . hashWith Blake2b_512 .
-           BL.toStrict . BL.toLazyByteString . BL.word64HexFixed $ n
+           BL.toStrict . BL.toLazyByteString $ BL.byteString ns <> BL.word64HexFixed n
       t1 = byteStringToElement (render bs)
       t2 = byteStringToElement (render $ BS.take 4 bs)
       t3 = byteStringToElement (render $ BS.take 2 bs)
   in (bs, t1, t2, t3)
 
-bruteforce :: C.Element -> IO BS.ByteString
-bruteforce e = go 0
+bruteforce :: BS.ByteString -> C.Element -> IO BS.ByteString
+bruteforce ns e = go 0
   where
     go :: Word64 -> IO BS.ByteString
     go !n = do
-      let (bs, t1, t2, t3) = tryOnce n
-      when (n `mod` 0x100 == 0) $ output n bs (C.atomicNumber t1) (C.atomicNumber t2) (C.atomicNumber t3)
+      let (bs, t1, t2, t3) = tryOnce ns n
+      when (n `mod` 0x1000 == 0) $ output n bs (C.atomicNumber t1) (C.atomicNumber t2) (C.atomicNumber t3)
       if (e == t1) && (e == t2) && (e == t3)
         then output n bs (C.atomicNumber t1) (C.atomicNumber t2) (C.atomicNumber t3) >> pure bs
         else go (n+1)
 
-data Mode = Hash BS.ByteString | Bruteforce String
+data Mode = Hash BS.ByteString | Bruteforce BS.ByteString String
 
 run :: Mode -> IO ()
 run (Hash a) = let el = byteStringToElement a
                in printf "%s = %3d %s\n" (decodeUtf8 a) (C.atomicNumber el) (C.name el)
 
-run (Bruteforce e_symb) = do
+run (Bruteforce ns e_symb) = do
   e <- case C.elementBySymbol e_symb of
     C.Unknown -> do printf "I don't know about %s.\n" e_symb
                     exitFailure
     e' -> pure e'
-  bs <- bruteforce e
+  bs <- bruteforce ns e
   printf "\n"
   run . Hash . render $ bs
 
@@ -85,8 +85,8 @@ hashOptions = Hash <$>
   argument str (metavar "<data>")
 
 searchOptions :: Parser Mode
-searchOptions = Bruteforce <$>
-   argument str (metavar "<element>")
+searchOptions = Bruteforce <$> argument str (metavar "<namespace>")
+                           <*> argument str (metavar "<element>")
 
 opts :: Parser Mode
 opts =
