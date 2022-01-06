6316c6270a573e366027fde3fb67c4f9bf992e22
Viktor Kleen <viktor@kleen.org>
2022-01-06 13:34:19 +0000

    Add quiet mode

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,9 @@
-{-# language BangPatterns #-}
+{-# language BangPatterns, NamedFieldPuns, RecordWildCards, ApplicativeDo #-}
 module Main where
 
 import           Control.Category ((>>>))
-import           Control.Monad (when)
+import           Control.Monad (when, unless)
 import           Crypto.Hash
-import           Crypto.Random (MonadRandom(..))
 import           Data.Bits
 import           Data.Word (Word64)
 import           Data.ByteArray (convert)
@@ -29,9 +28,6 @@ byteStringToElement =     hashWith Blake2b_512
                       >>> (+ 1)
                       >>> C.element
 
-randomBytes :: MonadRandom m => m BS.ByteString
-randomBytes = getRandomBytes 16
-
 render :: BS.ByteString -> BS.ByteString
 render = BL.byteStringHex >>> BL.toLazyByteString >>> BL.toStrict
 
@@ -54,31 +50,41 @@ tryOnce ns n =
       t3 = byteStringToElement (render $ BS.take 2 bs)
   in (bs, t1, t2, t3)
 
-bruteforce :: BS.ByteString -> C.Element -> IO BS.ByteString
-bruteforce ns e = go 0
+bruteforce :: Bool -> BS.ByteString -> C.Element -> IO BS.ByteString
+bruteforce quiet ns e = go 0
   where
     go :: Word64 -> IO BS.ByteString
     go !n = do
       let (bs, t1, t2, t3) = tryOnce ns n
-      when (n `mod` 0x1000 == 0) $ output n bs (C.atomicNumber t1) (C.atomicNumber t2) (C.atomicNumber t3)
+      unless quiet $ when (n `mod` 0x100 == 0 && n `mod` 0xff == 0) $
+        output n bs (C.atomicNumber t1) (C.atomicNumber t2) (C.atomicNumber t3)
       if (e == t1) && (e == t2) && (e == t3)
-        then output n bs (C.atomicNumber t1) (C.atomicNumber t2) (C.atomicNumber t3) >> pure bs
+        then do
+          unless quiet $ output n bs (C.atomicNumber t1) (C.atomicNumber t2) (C.atomicNumber t3)
+          pure bs
         else go (n+1)
 
 data Mode = Hash BS.ByteString | Bruteforce BS.ByteString String
 
-run :: Mode -> IO ()
-run (Hash a) = let el = byteStringToElement a
-               in printf "%s = %3d %s\n" (decodeUtf8 a) (C.atomicNumber el) (C.name el)
+data Args = Args { quiet :: Bool
+                 , mode :: Mode
+                 }
 
-run (Bruteforce ns e_symb) = do
+run :: Args -> IO ()
+run Args { quiet, mode = Hash a } =
+  let el = byteStringToElement a
+  in case quiet of
+    True -> printf "%s\n" $ decodeUtf8 a
+    False -> printf "%s = %3d %s\n" (decodeUtf8 a) (C.atomicNumber el) (C.name el)
+
+run a@Args { quiet, mode = Bruteforce ns e_symb } = do
   e <- case C.elementBySymbol e_symb of
     C.Unknown -> do printf "I don't know about %s.\n" e_symb
                     exitFailure
     e' -> pure e'
-  bs <- bruteforce ns e
-  printf "\n"
-  run . Hash . render $ bs
+  bs <- bruteforce quiet ns e
+  unless quiet $ printf "\n"
+  run $ a { mode = Hash . render $ bs }
 
 hashOptions :: Parser Mode
 hashOptions = Hash <$>
@@ -88,14 +94,23 @@ searchOptions :: Parser Mode
 searchOptions = Bruteforce <$> argument str (metavar "<namespace>")
                            <*> argument str (metavar "<element>")
 
-opts :: Parser Mode
-opts =
+parseMode :: Parser Mode
+parseMode =
   hsubparser
     (  command "hash" (info hashOptions
          (progDesc "Hash a string into an element using Blake2b_512."))
     <> command "search" (info searchOptions
          (progDesc "Find a hexadecimal string of length 32 such that it, its 8 character prefix and its 4 character prefix hash to a given element."))
     )
+
+opts :: Parser Args
+opts = do
+  quiet <- switch (  long "quiet"
+                  <> short 'q'
+                  <> help "Be quiet and only print the full result"
+                  )
+  mode <- parseMode
+  pure Args {..}
 
 main :: IO ()
 main = customExecParser (prefs showHelpOnError) o >>= run
