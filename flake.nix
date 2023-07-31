8558c6f9915ce3d83849ee8cc2a7dbd5ddfc9e4b
Viktor Kleen <viktor@kleen.org>
2023-07-31 21:57:18 +0000

    nix flake update

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
 
       pkgs = forAllSystems' (system: pkgs: pkgs.appendOverlays (overlays system));
 
-      ghcVersion = "961";
+      ghcVersion = "94";
 
       src = inputs.nix-filter.lib {
         root = ./.;
